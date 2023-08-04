c2909abe2db3c3c951f4b3374cfdd8a8c1ea6d5c
Michael <michaellwhitehill@gmail.com>
2023-08-04 01:22:09 -0600

    Made configs dynamic and moved all data to appdata (#6)

---
+++ image_libraries/image_library.py
@@ -0,0 +1,71 @@
+import json
+import requests
+import os
+from earthview_scraper import earthview_scraper
+import shutil
+
+LIB_PATH_KEY = "library_path"
+NEXT_IMAGE_POINTER_KEY = "earthview_pointer"
+LOCAL_IMAGES_KEY = "local_paths"
+LIB_DATA_PATH_KEY = "lib_data_path"
+DOWNLOAD_PATH_KEY = "download_path"
+class ImageLibrary():
+    def __init__(self, lib_config: dict) -> None:
+        self.config = lib_config
+        self.load_config()
+        image_data_handler = open(self.config[LIB_DATA_PATH_KEY], encoding="utf-8")
+        self.image_data = json.load(image_data_handler)
+        image_data_handler.close()
+        self.download_path = self.config[DOWNLOAD_PATH_KEY]
+    
+    def load_config(self):
+        if None is self.config.get(LIB_PATH_KEY, None):
+            self.config[LIB_PATH_KEY] = os.path.join(os.getenv("LOCALAPPDATA"), "bg_changer", "earthview_lib")
+        if None is self.config.get(DOWNLOAD_PATH_KEY, None):
+            self.config[DOWNLOAD_PATH_KEY] = os.path.join(self.config[LIB_PATH_KEY], "downloads")
+        if None is self.config.get(LIB_DATA_PATH_KEY, None):
+            self.config[LIB_DATA_PATH_KEY] = os.path.join(self.config[LIB_PATH_KEY], "data.json")
+        if None is self.config.get(NEXT_IMAGE_POINTER_KEY, None):
+            self.config[NEXT_IMAGE_POINTER_KEY] = 0
+        # check that paths and files exist
+        if not os.path.isdir(self.config[LIB_PATH_KEY]):
+            os.mkdir(self.config[LIB_PATH_KEY])
+        if not os.path.isfile(self.config[LIB_DATA_PATH_KEY]):
+            print("Earthview data is missing")
+            will_fetch = input("Would you like to use the prebuilt earthview library index? (y/n)")
+            if "y" == will_fetch:
+                # copy relative path to earthview results to data path
+                shutil.copy(r"earthview_scraper\earthview_data.json", self.config[LIB_DATA_PATH_KEY])
+            else:
+                print("Collecting Earthview data")
+                locations = earthview_scraper.collect_all_locations()
+                earthview_scraper.print_locations_to_file(locations, self.config[LIB_DATA_PATH_KEY])
+                print("earthview data collected")
+    
+    def get_config(self):
+        return self.config
+
+
+    def next(self) -> str:
+        # TODO: make network and io operations async
+        # download next image
+        # remove current image
+        # return local image path
+        self.download_image(self.config[NEXT_IMAGE_POINTER_KEY])
+        bg = self.image_data[self.config[NEXT_IMAGE_POINTER_KEY]]
+        self.config[NEXT_IMAGE_POINTER_KEY] += 1
+        return bg["local_path"]
+
+    def cleanup(self) -> str:
+        prev_image = self.config[NEXT_IMAGE_POINTER_KEY]
+
+    def download_image(self, index: int):
+        img_bytes = requests.get(self.image_data[index]["photoUrl"]).content
+        img_name = self.image_data[index]["name"].strip()+".jpg"
+        img_path = os.path.join(self.download_path, img_name)
+        if not os.path.exists(self.download_path):
+            os.makedirs(self.download_path)
+        with open(img_path, 'wb') as handler:
+            handler.write(img_bytes)
+        self.image_data[index]["local_path"]= img_path
+
--- main.py
+++ main.py
@@ -1,68 +1,74 @@
 import os
 import time
 import yaml
-import subprocess
-from image_library import Image_Library
+from image_libraries.image_library import ImageLibrary
 from earthview_scraper import earthview_scraper
 from wallpaper_changer.idesktop import IDesktopWallpaper
 
-
-
-CONFIG_PATH = "config.yaml"
-
-EATHVIEW_DATA_PATH_KEY = "earthview_data_path"
-EARTHVIEW_CONFIG_PATH_KEY = "earthview_config_path"
+EARTHVIEW_CONFIG_KEY = "earthview_config"
 MONITORS_KEY = "monitors"
-
-WALLPAPER_CHANGER_PATH = r"wallpaper_changer\bin\Release\net7.0\publish\DesktopWallpaperSample.exe"
 
 class BackgroundManager():
 
     def __init__(self) -> None:
-        self.config_path = CONFIG_PATH
+        self.config_path = os.path.join(os.getenv("LOCALAPPDATA"), "bg_changer", "config.yml")
         self.desktop = IDesktopWallpaper.CoCreateInstance()
+        self.earthview_lib = None
         self.load_config()
+        self.earthview_lib = ImageLibrary(self.config.get(EARTHVIEW_CONFIG_KEY, {}))
 
-        if not os.path.isfile(self.config[EATHVIEW_DATA_PATH_KEY]):
-            print("Earthview data is missing")
-            print("Collecting Earthview data")
-            locations = earthview_scraper.collect_all_locations()
-            earthview_scraper.print_locations_to_file(locations, self.config[EATHVIEW_DATA_PATH_KEY])
-        else:
-            print("Earthview data is present")
-        self.earthview_lib = Image_Library(self.config[EARTHVIEW_CONFIG_PATH_KEY], self.config[EATHVIEW_DATA_PATH_KEY])
-        for monitor in self.config[MONITORS_KEY]:
-            path = self.earthview_lib.next()
-            self.change_background(monitor, path)
     
     def load_config(self) -> None:
+        if not os.path.isfile(self.config_path):
+            self.create_config()
         config_file_handler = open(self.config_path)
         self.config = yaml.safe_load(config_file_handler)
         config_file_handler.close()
         if None is self.config.get(MONITORS_KEY, None):
             self.config[MONITORS_KEY] = self.get_monitors()
 
+    def create_config(self) -> None:
+        self.config = {}
+        self.config[EARTHVIEW_CONFIG_KEY] = None
+        if not os.path.isdir(os.path.join(os.getenv("LOCALAPPDATA"), "bg_changer")):
+            os.mkdir(os.path.join(os.getenv("LOCALAPPDATA"), "bg_changer"))
+        self.save_config()
+        
+
     def save_config(self) -> None:
-        os.remove(CONFIG_PATH)
-        with open(CONFIG_PATH, 'w') as file:
+        if None is not self.earthview_lib:
+            self.config[EARTHVIEW_CONFIG_KEY] = self.earthview_lib.get_config()
+        else:
+            self.config[EARTHVIEW_CONFIG_KEY] = {}
+        if os.path.isfile(self.config_path):
+            os.remove(self.config_path)
+        with open(self.config_path, 'w') as file:
             yaml.dump(self.config, file)
-        self.earthview_lib.save_config()
 
     def get_monitors(self) -> list[str]:
         mon_count = self.desktop.GetMonitorDevicePathCount()
         monitors = []
         for i in range(0, mon_count):
+            if self.desktop.GetMonitorDevicePathAt(i) == "":
+                continue
             monitors.append(self.desktop.GetMonitorDevicePathAt(i))
         return monitors
 
+    def cycle_monitors(self):
+        for monitor in self.config[MONITORS_KEY]:
+            background =  self.earthview_lib.next()
+            print(f"mon: {monitor} bg: {background}")
+            self.change_background(monitor, background)
+
     def change_background(self, monitorId: str, bg_path: str) -> None:
         self.desktop.SetWallpaper(monitorId=monitorId, wallpaper=bg_path)
-        time.sleep(1.5)
+        time.sleep(2)
         
 
 
 def main():
     bm = BackgroundManager()
+    bm.cycle_monitors()
     bm.save_config()
 
 
